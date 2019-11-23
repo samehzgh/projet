fa085ad6092e57af25cd47ea838e707f93d9b716
samehzgh <samehzgh@gmail.com>
2019-11-23 11:54:56 +0100

    rename Index.html TO index.html

--- src/test/java/com/someza/ams/controllers/FirstControllerTest.java
+++ src/test/java/com/someza/ams/controllers/FirstControllerTest.java
@@ -1,8 +1,30 @@
 package com.someza.ams.controllers;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 @WebMvcTest
 public class FirstControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test 
+	public void Test1_home() throws Exception
+	{
+		this.mockMvc.perform(get("/home")).andExpect(status().isOk())
+		.andExpect((ResultMatcher) containsString("favorite"));
+	}
+	
+	
+	
 }
